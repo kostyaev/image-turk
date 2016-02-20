4beb8639f3dc3feac2eef3e79378cf4903ebdc6c
Dmitry Kostyaev <d.kostyaev@babo.com>
2016-02-20 16:32:04 +0300

    Add simple paging

--- web/views.py
+++ web/views.py
@@ -17,41 +17,6 @@ google_searcher = imsearchtools.query.GoogleWebSearch()
 def ping():
     return "ok"
 
-@app.route("/query/<keywords>", methods=["GET"])
-def query(keywords):
-    images = google_searcher.query(keywords)
-    return render_template("query.html",
-                           title='Home',
-                           images=images)
-
-
-@app.route("/images", methods=["GET"])
-def get_images():
-    images =[f for f in listdir(static_dir) if (f.endswith(".jpg"))]
-    return render_template("images.html",
-                           title='Home',
-                           images=images)
-
-
-@app.route("/images", methods=["POST"])
-def upload_image():
-    url = request.json['url']
-    response = jsonify({})
-    if '.gif' not in url:
-        data = urllib.urlopen(url).read()
-        if len(data) > 10000:
-            with open(static_dir + str(uuid.uuid4()) + ".jpg", 'w') as f:
-                f.write(data)
-    else:
-        response.status_code = 400
-    return response
-
-@app.route("/images/:id", methods=["DELETE"])
-def delete_image():
-    return "ok"
-
-
-
 
 @app.route("/browse", defaults={'relative_path': ""})
 @app.route("/browse/<path:relative_path>", methods=["GET"])
@@ -60,21 +25,25 @@ def list_dirs(relative_path):
     all_files = listdir(path)
     dirs = [f for f in all_files if os.path.isdir(join(path, f))]
     relative_path = "/" if relative_path == "" else "/" + relative_path + "/"
-    images = [relative_path + f for f in all_files if f.endswith(".jpg")]
+    images = [relative_path + f for f in all_files if f.endswith(".jpg") or f.endswith(".JPEG")]
     return render_template("browse.html",
                            title='Browse',
                            dirs=dirs,
-                           images=images)
+                           images=images,
+                           total=len(images) + len(dirs))
 
 
 @app.route("/browse", defaults={'relative_path': ''}, methods=["POST"])
 @app.route("/browse/<path:relative_path>", methods=["POST"])
 def query_page(relative_path):
     q = request.form['query']
-    images = google_searcher.query(q)
+    max = int(request.form['max'])
+    skip = int(request.form['skip'])
+    images = google_searcher.query(q, num_results=max)[skip:]
     return render_template("query.html",
                            title='Home',
-                           images=images)
+                           images=images,
+                           total=len(images))
 
 
 @app.route("/browse", defaults={'relative_path': ''}, methods=["PUT"])
