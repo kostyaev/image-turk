cf46ed9184055e182c22abfa3cd192468d537947
Dmitry Kostyaev <d.kostyaev@babo.com>
2016-07-18 13:16:40 +0300

    Minor fix

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ from pyhocon import ConfigFactory
 
 conf = ConfigFactory.parse_file("application.conf")
 
-static_dir = conf.get("static_dir")
+static_dir = conf.get("static_dir").rstrip('/') + '/'
--- web/api/general.py
+++ web/api/general.py
@@ -16,14 +16,13 @@ bing_searcher = None
 instagram_searcher = None
 flickr_searcher = None
 
-static_dir = u"/Users/dmitry/Pictures/"
 
 @app.route("/ping", methods=["GET"])
 def ping():
     return "ok"
 
 def join(a,b):
-    return os.path.join(static_dir, a, b).lstrip(static_dir)
+    return os.path.join(static_dir, a, b).replace(static_dir, '')
 
 def listdir(d):
     return os.listdir(os.path.join(static_dir, d))
@@ -32,15 +31,17 @@ def isdir(d):
     return os.path.isdir(os.path.join(static_dir, d))
 
 
-@app.route("/api/dirs/<path:id>", methods=["GET"])
-def get_dir_by_id(id):
-    all_files = [join(id, f) for f in listdir(id)]
-    child_dirs = [{'id': f, 'name': f.rsplit('/', 1)[1]} for f in all_files if isdir(f)]
-    images = [{'id': f.rsplit('/', 1)[1], 'url': f} for f in all_files if f.lower().endswith('.jpg')]
-    parent_dir_id = id.rstrip('/').rsplit('/', 1)[0]
-    parent_dir_id = '' if parent_dir_id == id else parent_dir_id
+
+@app.route("/api/dirs/<path:path_id>", methods=["GET"])
+def get_dir_by_id(path_id):
+    all_files = [join(path_id, f) for f in listdir(path_id)]
+
+    child_dirs = [{'id': f, 'name': f.rsplit('/', 1)[-1]} for f in all_files if isdir(f)]
+    images = [{'id': f.rsplit('/', 1)[-1], 'url': f} for f in all_files if f.lower().endswith('.jpg')]
+    parent_dir_id = path_id.rstrip('/').rsplit('/', 1)[0]
+    parent_dir_id = '' if parent_dir_id == path_id else parent_dir_id
     siblings_dirs = [{'id': join(parent_dir_id, f), 'name': f} for f in listdir(parent_dir_id) if isdir(join(parent_dir_id, f))]
-    response = {'id': id, 'name': id.rstrip('/').split('/')[-1], 'images': images,
+    response = {'id': path_id, 'name': path_id.rstrip('/').split('/')[-1], 'images': images,
                 'parent': parent_dir_id, 'children': child_dirs, 'siblings': siblings_dirs}
     return jsonify(response)
 
