3551ef525253405cab65cdc85b63c9334b113b89
dkostyaev <dmitry@kostyaev.me>
2019-06-10 17:16:15 +0300

    multiple static folders

--- web/__init__.py
+++ web/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, Blueprint
 from config import *
 
-app = Flask(__name__, static_url_path="/viewer", static_folder=static_dir)
+app = Flask(__name__, static_url_path="", static_folder=static_dir)
 
 blueprint = Blueprint('site', __name__, static_folder='static/site')
 app.register_blueprint(blueprint)
